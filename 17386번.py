35c0c772613a768c89d7b711dc6810d2ee71a5b5
TYDTYD <tmdals5587@naver.com>
2022-01-29 22:32:28 +0900

    first commit

--- 17386번.py
+++ 17386번.py
@@ -7,11 +7,7 @@ def solution():
     ccw341=ccw(x3,y3,x4,y4,x1,y1)
     ccw342=ccw(x3,y3,x4,y4,x2,y2)
 
-    if ccw123*ccw124==0 and ccw341*ccw342==0:
-        if min(x1,x2)<=max(x3,x4) and min(x3,x4)<=max(x1,x2) and min(y1,y2)<=max(y3,y4) and min(y3,y4)<=max(y1,y2):
-            return 1
-    else:
-        if ccw123*ccw124<=0 and ccw341*ccw342<=0:
+    if ccw123*ccw124<=0 and ccw341*ccw342<=0:
             return 1
     return 0
 
