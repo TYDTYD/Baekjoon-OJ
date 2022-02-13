a22e8138a0d2768824fbd015c62f22e7e0cc21ed
TYDTYD <tmdals5587@naver.com>
2022-02-14 00:32:21 +0900

    third commit

--- 9527번.py
+++ 9527번.py
@@ -1,13 +1,21 @@
 import sys
-import math
 input=sys.stdin.readline
 
+def one(x):
+    ans=x&1
+    for i in range(54,0,-1):
+        if x&2**i:
+            ans+=count[i-1]+(x-(2**i)+1)
+            x-=2**i
+    return ans
+
 a,b=map(int,input().split())
-count=0
+count=[0]*55
+count[0]=1
 
-for i in range(54):
-    if b//2**i==0 and count==0:
-        count=i
-        break
+for i in range(1,55):
+    count[i]=2*count[i-1]+2**i
 
-print(count,2**count)
+answer=one(b)-one(a-1)
+
+print(answer)
