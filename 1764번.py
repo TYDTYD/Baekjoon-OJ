25be93f5b9b49e1a795aa734c2b602844fb74195
TYDTYD <tmdals5587@naver.com>
2022-02-14 23:07:30 +0900

    first commit

--- 1764번.py
+++ 1764번.py
@@ -1,10 +1,10 @@
 import sys
 
-n, m = map(int, sys.stdin.readline().split())
-not_heard = [sys.stdin.readline().strip() for _ in range(n)]
-never_seen = [sys.stdin.readline().strip() for _ in range(m)]
+n,m=map(int, sys.stdin.readline().split())
+index1=set([sys.stdin.readline().strip() for _ in range(n)])
+index2=set([sys.stdin.readline().strip() for _ in range(m)])
 
-result = sorted(list(not_heard & never_seen))
+result=sorted(list(index1&index2))
 
 print(len(result))
 for i in result:
